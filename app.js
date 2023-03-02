ccbaaca14d6e3a370d2b38f7db3c4e9c6bab75af
edgardoll4 <edgardoll4@gmail.com>
2023-03-01 22:57:20 -0400

    agregando nuevos flujos para probar consumir api

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
 
+const { flowGracias, flowTasa, flowInfo, flowFormulario, flowStatistics } = require('./flujo-secundarios')
+
 const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MySQLAdapter = require('@bot-whatsapp/database/mysql')
@@ -24,133 +26,33 @@ const MYSQL_DB_PORT = 7011
  *
  * Primero declaras los submenus 1.1 y 2.1, luego el 1 y 2 y al final el principal.
  */
-const flowFormulario = addKeyword(['formulario'])
-    .addAnswer(
-        ['Hola!', 'Escriba su *Nombre* para generar su solicitud'], { capture: true, buttons: [{ body: '❌ Cancelar solicitud' }] },
-        async(ctx, { flowDynamic, endFlow }) => {
-            if (ctx.body == '❌ Cancelar solicitud') {
-                await flowDynamic([{
-                    body: '❌ *Su solicitud de cita ha sido cancelada* ❌',
-                    buttons: [{ body: '⬅️ Volver al Inicio' }],
-                }, ])
-                return endFlow()
-            }
-        }
-    )
-    .addAnswer(
-        ['También necesito tus dos apellidos'], { capture: true, buttons: [{ body: '❌ Cancelar solicitud' }] },
-        async(ctx, { flowDynamic, endFlow }) => {
-            if (ctx.body == '❌ Cancelar solicitud') {
-                await flowDynamic([{
-                    body: '❌ *Su solicitud de cita ha sido cancelada* ❌',
-                    buttons: [{ body: '⬅️ Volver al Inicio' }],
-                }, ])
-                return endFlow()
-            }
-        }
-    )
-    .addAnswer(
-        ['Dejeme su número de teléfono y le llamaré lo antes posible.'], { capture: true, buttons: [{ body: '❌ Cancelar solicitud' }] },
-        async(ctx, { flowDynamic, endFlow }) => {
-            if (ctx.body == '❌ Cancelar solicitud') {
-                await flowDynamic([{
-                    body: '❌ *Su solicitud de cita ha sido cancelada* ❌',
-                    buttons: [{ body: '⬅️ Volver al Inicio' }],
-                }, ])
-                return endFlow()
-            }
-        }
-    )
 
-const flowStringBtn = addKeyword('btn', 'boton', 'botón').addAnswer('Este mensaje envia tres botones', {
-    buttons: [{
-            body: 'Boton 1',
-            id: 'token-asd'
-        },
-        {
-            body: 'Boton 2',
-            id: 'token-123'
-        },
-        {
-            body: 'Boton 3',
-            id: 'token-xyz'
-        },
-        {
-            body: 'Boton 4',
-            id: 'token-abc'
-        }
-    ],
-    capture: true,
-    delay: (0)
-})
-
-const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
-
-const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
-    [
-        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
-        'https://bot-whatsapp.netlify.app/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null, [flowSecundario]
-)
-
-const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
-    [
-        '🙌 Aquí encontras un ejemplo rapido',
-        'https://bot-whatsapp.netlify.app/docs/example/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null, [flowSecundario]
-)
-
-const flowGracias = addKeyword(['gracias', 'grac', 'salir']).addAnswer(
-    [
-        '🚀 Puedes aportar tu granito de arena a este proyecto',
-        '[*opencollective*] https://opencollective.com/bot-whatsapp',
-        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
-        '[*patreon*] https://www.patreon.com/leifermendez',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null, [flowSecundario]
-)
-
-const flowDiscord = addKeyword(['discord']).addAnswer(
-    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
-    null,
-    null, [flowSecundario]
-)
-
-const flowImg = addKeyword(['imagen', 'img']).addAnswer('Este mensaje envia una imagen', {
-    media: 'https://content.app-sources.com/s/71425562438133975/uploads/Images/Foto4-9413559.png',
-}).addAnswer('Para volver al fluo principal', {
-    buttons: [{
-            body: 'regresar',
-            id: 'token-asd',
-        },
-        {
-            body: 'salir',
-            id: 'token-123',
-        }
-    ],
-    capture: true,
-    delay: (0)
-}, null, [flowGracias])
-
-const flowPrincipal = addKeyword(['halo', 'ole', 'alo', 'Volver', 'regresar', 'volver al flujo principal'])
+const flowPrincipal = addKeyword(['halo', 'ole', 'alo', 'Volver', 'regresar', 'menu', 'menu principal'])
     .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
     .addAnswer(
         [
-            'te comparto los siguientes links de interes sobre el proyecto',
-            '',
-            'bienvenido',
+            'Hola soy Keo su asistente virtual de Planner',
+            '\nBienvenido estimado clinte',
         ],
         null,
-        null, [flowDocs, flowGracias, flowTuto, flowDiscord, flowImg]
-    )
+        null, [flowGracias]
+    ).addAnswer(['Tenemos las siguientes opciones para seguir interactuando'], {
+        buttons: [{
+                body: 'Saber la Tasa del día',
+                id: 'token-tasa'
+            },
+            {
+                body: 'Información sbre Keoplanner',
+                id: 'token-info'
+            },
+            {
+                body: 'Salir',
+                id: 'token-salir'
+            }
+        ],
+        capture: true,
+        delay: (0)
+    },null,[flowGracias, flowInfo, flowTasa])
 
 const main = async() => {
     const adapterDB = new MySQLAdapter({
@@ -160,7 +62,7 @@ const main = async() => {
         password: MYSQL_DB_PASSWORD,
         port: MYSQL_DB_PORT
     })
-    const adapterFlow = createFlow([flowPrincipal, flowStringBtn, flowFormulario])
+    const adapterFlow = createFlow([flowPrincipal, flowFormulario, flowAdmin])
     const adapterProvider = createProvider(BaileysProvider)
     createBot({
         flow: adapterFlow,
---
+++ flujo-secundarios.js
@@ -0,0 +1,189 @@
+const{ addKeyword } = require('@bot-whatsapp/bot')
+
+const flowGracias = addKeyword(['gracias', 'grac', 'salir']).addAnswer(
+    [
+        '🚀 Puedes probar nuestro periodo de prueba',
+        '\n[*Keo planner*] https://www.keoplanner.com',
+    ],
+    {media:'https://www.keoplanner.com/assets/img/planner/logo.svg'},
+    null
+)
+
+const flowImgTasa = addKeyword(['imagen', 'img']).addAnswer('Este mensaje envia una imagen', {
+    media: 'https://content.app-sources.com/s/71425562438133975/uploads/Images/Foto4-9413559.png',
+}).addAnswer('Para volver al fluo principal', {
+    buttons: [{
+            body: 'menu principal',
+            id: 'token-menu',
+        },
+        {
+            body: 'salir',
+            id: 'token-salir',
+        }
+    ],
+    capture: true,
+    delay: (0)
+}, null, [flowGracias])
+
+const flowTasa = addKeyword(['Tasa','tasa']).addAnswer(
+    ['Elija un país:','Venezuela','Perú','Chile','Argentina','Colombia','Ecuador', 'Otro', '\n Para siguiente saber la tasa del mismo.'],
+    {
+        buttons: [{
+                body: 'menu principal',
+                id: 'token-menu',
+            },
+            {
+                body: 'salir',
+                id: 'token-salir',
+            }
+        ],
+        capture: true,
+        delay: (0)
+    },
+    null, [flowGracias]
+)
+
+const flowImg = addKeyword(['imagen', 'img']).addAnswer('Este mensaje envia una imagen', {
+    media: 'https://content.app-sources.com/s/71425562438133975/uploads/Images/Foto4-9413559.png',
+}).addAnswer('Para volver al fluo principal', {
+    buttons: [{
+            body: 'regresar',
+            id: 'token-menu',
+        },
+        {
+            body: 'salir',
+            id: 'token-salir',
+        }
+    ],
+    capture: true,
+    delay: (0)
+}, null, [flowGracias, flowImgTasa])
+
+const flowStatistics = addKeyword(['estadistica', 'metrica'])
+    .addAnswer(['*Estadisticas de los ultimos 7 días*'],
+    {
+        media:'https://mpnecuador.files.wordpress.com/2014/06/website-analytics.png',
+        buttons: [
+            {
+                body: 'salir',
+                id: 'token-salir',
+            }
+        ]
+    },
+    async (ctx,{flowDynamic}) =>{
+        dataInit = new Date();
+        dataEnd = dateInit[Symbol.toPrimitive]('number');
+        okPlanner = '';
+        errorPlanner = '';
+        msgUser = '';
+        total = '';
+        const statistic = await axios({
+            method:'get',
+            url:'https://api-ws-prod.herokuapp.com/api/chat/statistics-button-pressed/?end-time=' + dataEnd + '&start-time=' + (Number(dataEnd) - 604800000),
+        })
+
+        const statisticHour = await axios({
+            method:'get',
+            url:'https://api-ws-prod.herokuapp.com/api/chat/statistics/'+168,
+        })
+
+        if (statistic){
+            total = statistic.countConfirmar;
+            asistir = statistic.countAsistir;
+            anular = statistic.countAnular;
+            ambos = statistic.countAmbos;
+            sinAccion = total - asistir - anular - sinAccion;
+        }
+
+        if (statisticHour){
+            okPlanner = statisticHour.countSuccess;
+            errorPlanner = statisticHour.countFailBadRequest + statisticHour.countFailNotAcceptable + statisticHour.countFailNotFound + statisticHour.countFailUnauthorized + statisticHour.countFailConflict + statisticHour.countFailUnprocessableEntity + statisticHour.countFailOther;
+            msgUser = statisticHour.countMsgText;
+        }
+
+        return flowDynamic([`*Estadisticas de las plantillas de confirmaciones de reservas*
+            \nTotal de plantillas enviadas: ${total}
+            \n\nBotones presionados:
+            \nAsisti: ${asistir}
+            \nAnular: ${anular}
+            \nAmbos: ${ambos}
+            \nSin acciones: ${sinAccion}`,
+            `\n*staditicas del estatus de los botones presionados*,
+            \nOK: ${okPlanner}
+            \nError: ${errorPlanner}
+            \n\nCantidad de mensajes de textos escritos por el usuario: ${msgUser}`])
+
+    },[flowGracias]
+    )
+
+const flowInfo = addKeyword(['info', 'informacion']).addAnswer(
+    [
+        '🚀 Para más información le invito a ir al siguiente link.',
+        '\n[*Keo planner*] https://www.keoplanner.com',
+    ],
+    {media:'https://www.keoplanner.com/assets/img/planner/logo.svg'},
+    null
+)
+
+let nombre;
+let apellidos;
+let telefono;
+
+const flowFormulario = addKeyword(['Hola','⬅️ Volver al Inicio'])
+    .addAnswer(
+        ['Hola!','Para enviar el formulario necesito unos datos...' ,'Escriba su *Nombre*'],
+        { capture: true, buttons: [{ body: '❌ Cancelar solicitud' }] },
+
+        async (ctx, { flowDynamic, endFlow }) => {
+            if (ctx.body == '❌ Cancelar solicitud')
+             return endFlow({body: '❌ Su solicitud ha sido cancelada ❌',    // Aquí terminamos el flow si la condicion se comple
+                 buttons:[{body:'⬅️ Volver al Inicio' }]                      // Y además, añadimos un botón por si necesitas derivarlo a otro flow
+
+            
+            })
+            nombre = ctx.body
+            return flowDynamic(`Encantado *${nombre}*, continuamos...`)
+        }
+    )
+    .addAnswer(
+        ['También necesito tus dos apellidos'],
+        { capture: true, buttons: [{ body: '❌ Cancelar solicitud' }] },
+
+        async (ctx, { flowDynamic, endFlow }) => {
+            if (ctx.body == '❌ Cancelar solicitud') 
+                return endFlow({body: '❌ Su solicitud ha sido cancelada ❌',
+                    buttons:[{body:'⬅️ Volver al Inicio' }]
+
+
+        })
+        apellidos = ctx.body
+        return flowDynamic(`Perfecto *${nombre}*, por último...`)
+        }
+    )
+    .addAnswer(
+        ['Dejeme su número de teléfono y le llamaré lo antes posible.'],
+        { capture: true, buttons: [{ body: '❌ Cancelar solicitud' }] },
+
+        async (ctx, { flowDynamic, endFlow }) => {
+            if (ctx.body == '❌ Cancelar solicitud') 
+                return endFlow({body: '❌ Su solicitud ha sido cancelada ❌',
+                      buttons:[{body:'⬅️ Volver al Inicio' }]
+                })
+
+
+                telefono = ctx.body
+                await delay(2000)
+                return flowDynamic(`Estupendo *${nombre}*! te dejo el resumen de tu formulario
+                \n- Nombre y apellidos: *${nombre} ${apellidos}*
+                \n- Telefono: *${telefono}*`)
+        }
+    )
+
+module.exports = {
+    flowImg,
+    flowTasa,
+    flowGracias,
+    flowImgTasa,
+    flowFormulario,
+    flowStatistics
+}
